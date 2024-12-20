376f6c70d7c53f02b4d0143ab571b4e0202ef351
Jan Haans <jan.haans@gmail.com>
2024-12-20 23:15:12 +0100

    Updated SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
     print(SQL_SERVER)
     #SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+18+for+SQL+Server&encrypt=yes&trustServerCertificate=no&Connection+Timeout=30'
     SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc://adminuser:{SQL_PASSWORD}@animals-jh-db-server.database.windows.net:1433/animals-db?driver=ODBC+Driver+18+for+SQL+Server&encrypt=yes&trustServerCertificate=no"
-)
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT')
