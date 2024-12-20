8a98419cf0b5247d6e9600f556f557013d5167df
Jan Haans <jan.haans@gmail.com>
2024-12-20 16:27:51 +0100

    Added animals web app

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+
+    SQL_SERVER = os.environ.get('SQL_SERVER') 
+    SQL_DATABASE = os.environ.get('SQL_DATABASE')
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME')
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+18+for+SQL+Server'
+    #SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc://{SQL_USER_NAME}@{SQL_SERVER}:{SQL_PASSWORD}@{SQL_SERVER}:1433/{SQL_DATABASE}?driver=ODBC+Driver+18+for+SQL+Server"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT')
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY')
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER')
